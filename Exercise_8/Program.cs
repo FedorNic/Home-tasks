01ce1c2790533dae23f9e3541ba739770f2bb171
FedorNic <dmitrievfedor@yandex.ru>
2022-02-05 20:20:10 +0300

    Lesson 1 final

--- Exercise_8/Program.cs
+++ Exercise_8/Program.cs
@@ -4,5 +4,4 @@ string num1 = Console.ReadLine();
 int numer1 = Convert.ToInt32(num1);
 Console.WriteLine();
 for (int c = 2; c <= numer1; c = c + 2)
-    if (c % 2 == 0)
-        Console.WriteLine(c);
+    Console.WriteLine(c);
