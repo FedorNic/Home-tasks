f05841b7f154f760cabe0bcd51df5a20a0860f5d
FedorNic <dmitrievfedor@yandex.ru>
2022-02-17 19:47:03 +0300

    Lesson 5

---
+++ Exercise_36/Program.cs
@@ -0,0 +1,15 @@
+﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами.
+//Показать количество нечётных/чётных чисел.
+int[] mass = new int[8];
+int chot = 0, nechot = 0;
+for (int count = 0; count < mass.Length; count++)
+{
+    mass[count] = new Random().Next(100, 999);
+    Console.Write(mass[count] + "\t");
+    if (mass[count] % 2 == 0)
+    {
+        chot = chot + 1;
+    }
+    else nechot = nechot + 1;
+}
+Console.Write("\nЧисло четных = " + chot + "\tЧисло НЕ четных = " + nechot);
---
+++ Exercise_38/Program.cs
@@ -0,0 +1,14 @@
+﻿// 38. Найти сумму чисел одномерного массива, стоящих на нечётной позиции.
+int[] one = new int[10];
+int sum = 0;
+for (int poz = 0; poz < one.Length; poz++)
+{
+    one[poz] = new Random().Next(0, 6);
+    Console.Write(one[poz] + "\t");
+
+    if (poz % 2 != 0)
+    {
+        sum = sum + one[poz];
+    }
+}
+Console.Write("\nСумма чисел на нечетной позиции = " + sum);
