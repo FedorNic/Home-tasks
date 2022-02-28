c0a07d49d8a6bff40ea775bf5e9d57e1704c1258
FedorNic <dmitrievfedor@yandex.ru>
2022-02-28 19:45:10 +0300

    Lesson 6.1

--- Exercise_46/Program.cs
+++ Exercise_46/Program.cs
@@ -1,2 +1,13 @@
 ﻿// 46. Написать программу масштабирования фигуры
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Сколько точек нужно ввести?");
+int num = Convert.ToInt32(Console.ReadLine());
+int k = 2;
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine("Введите x = ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите y = ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Координаты точки - (" + x + "," + y + ")");
+    Console.WriteLine("Координаты точки масштабированной с коэффициентом 2 - (" + x*k + "," + y*k + ")");
+}
