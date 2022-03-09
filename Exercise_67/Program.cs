b0af4f81e5880b3e10f8bca05a54766b2685a28d
FedorNic <dmitrievfedor@yandex.ru>
2022-03-09 20:31:01 +0300

    09.03.2022

--- Exercise_67/Program.cs
+++ Exercise_67/Program.cs
@@ -1,2 +1,7 @@
 ﻿// 67. Показать натуральные числа от N до 1, N задано. N = 5. -> "5, 4, 3, 2, 1"
-Console.WriteLine("Hello, World!");
+string NumbersRec(int N)
+{
+    if (N < 1) return String.Empty;
+    return N + " " + NumbersRec(N - 1);
+}
+Console.WriteLine(NumbersRec(5));
--- Exercise_69/Program.cs
+++ Exercise_69/Program.cs
@@ -1,4 +1,9 @@
 ﻿// 69. Задайте значения M и N. Напишите программу, которая найдёт сумму элементов в промежутке от M до N.
 // M = 1; N = 4 -> 10
 // M = 4; N = 8. -> 30
-Console.WriteLine("Hello, World!");
+int NumbersSum(int M, int N)
+{
+    if (M > N) return 0;
+    return M + NumbersSum(M + 1, N);
+}
+Console.WriteLine(NumbersSum(4, 8));
